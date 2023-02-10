cafa2b0e151e678ba5ae27bbb3d65001172195bb
Ben Hofer <ben.hofer@gmail.com>
2023-02-10 13:20:08 -0500

    updated data url with new s3 bucket

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,125 +1,175 @@
-import React, {useState, useEffect} from 'react';
-import Card from '../components/Card';
-import axios from 'axios';
-import { BrowserRouter as Router, Switch, Route, Link, NavLink, useParams, useHistory, useLocation } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import Card from "../components/Card";
+import axios from "axios";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink,
+  useParams,
+  useHistory,
+  useLocation,
+} from "react-router-dom";
 
-const API_URL = "https://mjpsvgqnl7.execute-api.us-east-1.amazonaws.com/data"
+const API_URL = "/data.json";
 
 // A custom hook that builds on useLocation to parse
 // the query string for you.
 function useQuery() {
-    return new URLSearchParams(window.location.search);
-  }
-  
+  return new URLSearchParams(window.location.search);
+}
+
 function Browse(props) {
-  const [meditations, setMeditations] = useState('');
-  const [cardmm, setCardmm] = useState([0,4])
+  const [meditations, setMeditations] = useState("");
+  const [cardmm, setCardmm] = useState([0, 4]);
   const [currRow, setCurrRow] = useState(0);
   const [verse, setVerse] = useState(null);
   const [expand, setExpand] = useState(null);
 
   let location = useLocation();
-  let history = useHistory(); 
+  let history = useHistory();
   let query = useQuery();
   let filter = query.get("filter");
 
   const filterByWork = (cat) => {
     let newStructure = [];
-    cat.original_works.map(work => {
-      let newWork = [{...work},[]];
-      work.verses.map(id => {
-        newWork[1].push(cat.verses.filter(verse => verse.id === id)[0])
-      }) 
+    cat.original_works.map((work) => {
+      let newWork = [{ ...work }, []];
+      work.verses.map((id) => {
+        newWork[1].push(cat.verses.filter((verse) => verse.id === id)[0]);
+      });
       // console.log(newWork)
       newStructure.push(newWork);
-    })
+    });
     // console.info(newStructure)
-    return newStructure; 
-  }
+    return newStructure;
+  };
 
   const filterByAuthor = (cat) => {
     let newStructure = [];
-    cat.attribution.map(work => {
-      let newWork = [{...work},[]];
-      work.verses.map(id => {
-        newWork[1].push(...cat.verses.filter(verse => verse.id === id))
-      }) 
+    cat.attribution.map((work) => {
+      let newWork = [{ ...work }, []];
+      work.verses.map((id) => {
+        newWork[1].push(...cat.verses.filter((verse) => verse.id === id));
+      });
       // console.log(newWork)
       newStructure.push(newWork);
-    })
+    });
     // console.info(newStructure)
-    return newStructure; 
-  }
+    return newStructure;
+  };
 
   const filterByCollection = (cat) => {
     let newStructure = [];
-    cat.collections.map(work => {
+    cat.collections.map((work) => {
       // console.log('work', work)
-      let newWork = [{...work},[]];
-      work.verses.map(id => {
-        newWork[1].push(cat.verses.filter(verse => verse.id === id)[0])
-      }) 
+      let newWork = [{ ...work }, []];
+      work.verses.map((id) => {
+        newWork[1].push(cat.verses.filter((verse) => verse.id === id)[0]);
+      });
       // console.log(newWork)
       newStructure.push(newWork);
-    })
+    });
     // console.dir(newStructure)
-    return newStructure; 
-  }
+    return newStructure;
+  };
 
   useEffect(() => {
-    axios.get(API_URL)
+    axios
+      .get(API_URL)
       .then((response) => {
         let m;
-        if (filter === 'author') {
-          m = filterByAuthor(response.data)
-        } else if (filter === 'work') {
-          m = filterByWork(response.data)
-        } else if (filter === 'collection') {
-          m = filterByCollection(response.data)
+        if (filter === "author") {
+          m = filterByAuthor(response.data);
+        } else if (filter === "work") {
+          m = filterByWork(response.data);
+        } else if (filter === "collection") {
+          m = filterByCollection(response.data);
         }
         // console.log(m);
         setMeditations(m);
       })
       .catch((e) => {
-        console.log(e)
-      })
-  },[filter]);
+        console.log(e);
+      });
+  }, [filter]);
 
   return (
-    <div className="wrapper">
-    { meditations && meditations.map((cat,i) => 
-        <section className="o-section">
-          <div className="row">
-            <div className="col-md-3">
-              <Card title={cat[0].name} text={cat[0].description} id={cat[0].id} type="intro" introType={filter} />
-            </div>
-            <div className="col-md-9 meditation-cards">
-              <div className="row">
-                  { cat[1].map((verse,j) => (
-                      expand !== i ? j < 6 && (
-                        <div className="col-md-4">
-                          <Link to={'/app/meditate/'+verse.id}>
-                              <Card type="verse" title={verse.short_desc} subtitle={verse.attribution_hr} text={verse.text} url='link to meditation page' color='red' work={verse.work} />
-                          </Link>
-                        </div>
-                      ) : (
-                        <div className="col-md-4">
-                          <Link to={'/app/meditate/'+verse.id}>
-                              <Card type="verse" title={verse.short_desc} subtitle={verse.attribution_hr} text={verse.text} url='link to meditation page' color='red' work={verse.work} />
+    <div className='wrapper'>
+      {meditations &&
+        meditations.map((cat, i) => (
+          <section className='o-section'>
+            <div className='row'>
+              <div className='col-md-3'>
+                <Card
+                  title={cat[0].name}
+                  text={cat[0].description}
+                  id={cat[0].id}
+                  type='intro'
+                  introType={filter}
+                />
+              </div>
+              <div className='col-md-9 meditation-cards'>
+                <div className='row'>
+                  {cat[1].map((verse, j) =>
+                    expand !== i ? (
+                      j < 6 && (
+                        <div className='col-md-4'>
+                          <Link to={"/app/meditate/" + verse.id}>
+                            <Card
+                              type='verse'
+                              title={verse.short_desc}
+                              subtitle={verse.attribution_hr}
+                              text={verse.text}
+                              url='link to meditation page'
+                              color='red'
+                              work={verse.work}
+                            />
                           </Link>
                         </div>
                       )
+                    ) : (
+                      <div className='col-md-4'>
+                        <Link to={"/app/meditate/" + verse.id}>
+                          <Card
+                            type='verse'
+                            title={verse.short_desc}
+                            subtitle={verse.attribution_hr}
+                            text={verse.text}
+                            url='link to meditation page'
+                            color='red'
+                            work={verse.work}
+                          />
+                        </Link>
+                      </div>
                     )
                   )}
+                </div>
+                {expand !== i && cat[1].length > 6 && (
+                  <a
+                    href={"#" + cat[0].id}
+                    className='btn btn-full-width'
+                    onClick={() => setExpand(i)}
+                  >
+                    Show More
+                  </a>
+                )}
+                {expand === i && cat[1].length > 6 && (
+                  <a
+                    href={"#" + cat[0].id}
+                    className='btn btn-full-width'
+                    onClick={() => setExpand(null)}
+                  >
+                    Show Less
+                  </a>
+                )}
               </div>
-              { expand !== i && cat[1].length > 6 && <a href={'#'+cat[0].id} className='btn btn-full-width' onClick={() => setExpand(i)}>Show More</a> }
-              { expand === i && cat[1].length > 6 && <a href={'#'+cat[0].id} className='btn btn-full-width' onClick={() => setExpand(null)}>Show Less</a> }
             </div>
-          </div>
-        </section>
-    )}
-</div>
-  )
+          </section>
+        ))}
+    </div>
+  );
 }
 
 export default Browse;
--- src/pages/Meditation.js
+++ src/pages/Meditation.js
@@ -1,147 +1,173 @@
-import React, {useState, useRef, useEffect, Fragment} from 'react';
-import axios from 'axios';
-import {useParams, useHistory } from "react-router-dom";
-import styles from '../assets/css/pages/meditation.module.css';
-import { Icon } from '@iconify/react';
-import play from '@iconify-icons/mdi/play-circle';
-import back from '@iconify-icons/mdi/keyboard-backspace';
-import cross from '../assets/img/cross.svg';
+import React, { useState, useRef, useEffect, Fragment } from "react";
+import axios from "axios";
+import { useParams, useHistory } from "react-router-dom";
+import styles from "../assets/css/pages/meditation.module.css";
+import { Icon } from "@iconify/react";
+import play from "@iconify-icons/mdi/play-circle";
+import back from "@iconify-icons/mdi/keyboard-backspace";
+import cross from "../assets/img/cross.svg";
 
-const API_URL = "https://mjpsvgqnl7.execute-api.us-east-1.amazonaws.com/data"
-const BELL_URL = "https://s3.amazonaws.com/tempora-pray-web-bucket/bells/Ship_Bell_mono.mp3"
+const API_URL = "/data.json";
+const BELL_URL =
+  "https://s3.amazonaws.com/tempora-pray-web-bucket/bells/Ship_Bell_mono.mp3";
 
-function Meditation(props) { 
+function Meditation(props) {
+  const [verse, setVerse] = useState(null);
+  const [meditating, setMeditating] = useState(false);
+  // TODO - make length editable via UI
+  const [length, setLength] = useState(300000);
+  const [remaining, setRemaining] = useState(length - 1);
 
-    const [ verse, setVerse ] = useState(null);
-    const [ meditating, setMeditating ] = useState(false);
-    // TODO - make length editable via UI
-    const [ length, setLength ] = useState(300000);
-    const [ remaining, setRemaining ] = useState(length - 1);
+  // We'll need history to go back to the previous page
+  let history = useHistory();
+  const exit = () => {
+    history.goBack();
+  };
 
-    // We'll need history to go back to the previous page 
-    let history = useHistory();
-    const exit = () => {
-      history.goBack();
-    }
+  // Find the verse id from the params
+  let { id } = useParams();
+  let verseId = parseInt(id.replace(/\/app\/meditate\//, ""));
 
-    // Find the verse id from the params
-    let { id } = useParams();
-    let verseId = parseInt(id.replace(/\/app\/meditate\//, ''));
+  // Setup the useRef object for the timers
+  let audioTimer = useRef(null);
+  let bell1, audio, bell2;
 
-    // Setup the useRef object for the timers 
-    let audioTimer = useRef(null); 
-    let bell1, audio, bell2; 
+  // Play
+  function playSequence() {
+    // Setup audio elements
+    audioTimer.current = {};
+    audioTimer.current.verseAudio = new Audio(verse.url);
+    audioTimer.current.bell1Audio = new Audio(BELL_URL);
+    audioTimer.current.bell2Audio = new Audio(BELL_URL);
 
-    // Play 
-    function playSequence() {
-      // Setup audio elements
-      audioTimer.current = {};
-      audioTimer.current.verseAudio = new Audio(verse.url);
-      audioTimer.current.bell1Audio = new Audio(BELL_URL);
-      audioTimer.current.bell2Audio = new Audio(BELL_URL);
+    // get the current time and finish time
+    let t = new Date().getTime();
+    audioTimer.current.finish = t + length;
 
-      // get the current time and finish time
-      let t = new Date().getTime();
-      audioTimer.current.finish = t + length;
+    // create the audio sequence using callbacks
+    audioTimer.current.bell1Audio.addEventListener("ended", (event) => {
+      audioTimer.current.verseAudio.play();
+    });
+    audioTimer.current.verseAudio.addEventListener("ended", (event) => {
+      audioTimer.current.bell2Audio.play();
+    });
+    audioTimer.current.bell2Audio.addEventListener("ended", (event) => {
+      // audio sequence duration
+      let sequenceDuration =
+        (audioTimer.current.verseAudio.duration +
+          audioTimer.current.bell1Audio.duration +
+          audioTimer.current.bell1Audio.duration) *
+        1000;
+      // silence duration timer
+      audioTimer.current.silenceTimer = window.setTimeout(() => {
+        audioTimer.current.bell1Audio.play();
+      }, length - sequenceDuration * 2);
+    });
 
-      // create the audio sequence using callbacks 
-      audioTimer.current.bell1Audio.addEventListener('ended', (event) => {
-        audioTimer.current.verseAudio.play();
-      })
-      audioTimer.current.verseAudio.addEventListener('ended', (event) => {
-        audioTimer.current.bell2Audio.play(); 
-      })
-      audioTimer.current.bell2Audio.addEventListener('ended', (event) => {
-        let sequenceDuration = (audioTimer.current.verseAudio.duration + audioTimer.current.bell1Audio.duration + audioTimer.current.bell1Audio.duration) * 1000; 
-        audioTimer.current.silenceTimer = window.setTimeout(() => {audioTimer.current.bell1Audio.play()}, length - sequenceDuration*2);
-      })
+    // Begin the prayer
+    setMeditating(true);
+    audioTimer.current.bell1Audio.play();
 
-      // Begin the prayer
-      setMeditating(true);
-      audioTimer.current.bell1Audio.play();
-      
-      // Set interval to check the time every 30 seconds
-      audioTimer.current.intervalCountdown = window.setInterval(() => { 
-        let now = new Date().getTime(); 
-        let r = ( audioTimer.current.finish - now ); 
-        setRemaining(r) 
-      }, 30000);
+    // Set interval to check the time every 30 seconds
+    audioTimer.current.intervalCountdown = window.setInterval(() => {
+      let now = new Date().getTime();
+      let r = audioTimer.current.finish - now;
+      setRemaining(r);
+    }, 30000);
 
-      // Once the prayer is completed, clear timers and toggle the state
-      audioTimer.current.meditationCounter = window.setTimeout(() => { 
-        stopSequence(); 
-        setMeditating(false); 
-      }, length);
-    }
-
-    // Define the function to clear all the timers
-    function stopSequence() {
-      if (audioTimer && audioTimer.current) {
-        audioTimer.current.intervalCountdown && window.clearInterval(audioTimer.current.intervalCountdown);
-        audioTimer.current.silenceTimer && window.clearInterval(audioTimer.current.silenceTimer);
-        audioTimer.current.verseAudio && audioTimer.current.verseAudio.pause();
-        audioTimer.current.bell1Audio && audioTimer.current.bell1Audio.pause();
-        audioTimer.current.bell2Audio && audioTimer.current.bell2Audio.pause(); 
-      } 
-    }
-
-    // Run stop function on dismount
-    useEffect(() => {
-      return () => {
-        stopSequence();
-      }
-    }, [audio, bell1, bell2])
-
-    // API call runs on mount
-    useEffect(() => {
-      axios.get(API_URL)
-        .then((response) => {
-          let verseContent = response.data.verses.filter(verse => verse.id === verseId)[0]
-          setVerse(verseContent)
-        })
-        .catch((e) => {
-          console.log(e)
-        })
-    },[verseId]);
-  
-    return (
-      <div className={styles.wrapper}>
-          <button onClick={exit} className={styles.back_btn}>
-            <Icon icon={back} width="40px" color="white" />
-          </button>
-          <h1>{verse && verse.short_desc}</h1>
-          <p className={`${styles.attribution} subtitle1`}>{verse && verse["attribution_hr"]}</p>
-          <div className={styles.time}>
-            {
-              !meditating && <span>
-                { (length/60000) } minutes
-              </span>
-            }
-            {
-              meditating && <span>
-                { remaining < 60000 && 
-                  <Fragment>Less than 1 minute</Fragment>
-                }{ remaining > 60000 && 
-                   /* break out logic */
-                   <Fragment> { Math.floor(remaining/60000) } minute{Math.floor(remaining/60000) !== 1 && <span>s</span>  }</Fragment>
-                } remaining
-              </span>
-            }
-          </div>
-          { !meditating && 
-           /* onMouseEnter and onMouseLeave instead */
-            <button className={`${styles.play_btn} ${styles.meditate}`} onMouseOut={() => props.setEngagement('')} onMouseOver={() => {props.setEngagement('press')}} onClick={() => playSequence()}>
-              <Icon icon={play} width="80px" color="white" style={{ textAlign: 'center'}}/>
-            </button>
-          }{ meditating && 
-
-              <div className={`${styles.icon} ${styles.meditate}`} >
-                <img src={cross} alt='The Cross' width='100px' />
-              </div>              
-            
-          }
-      </div>
-    )
+    // Once the prayer is completed, clear timers and toggle the state
+    audioTimer.current.meditationCounter = window.setTimeout(() => {
+      stopSequence();
+      setMeditating(false);
+    }, length);
   }
 
-  export default Meditation;
+  // Define the function to clear all the timers
+  function stopSequence() {
+    if (audioTimer && audioTimer.current) {
+      audioTimer.current.intervalCountdown &&
+        window.clearInterval(audioTimer.current.intervalCountdown);
+      audioTimer.current.silenceTimer &&
+        window.clearInterval(audioTimer.current.silenceTimer);
+      audioTimer.current.verseAudio && audioTimer.current.verseAudio.pause();
+      audioTimer.current.bell1Audio && audioTimer.current.bell1Audio.pause();
+      audioTimer.current.bell2Audio && audioTimer.current.bell2Audio.pause();
+    }
+  }
+
+  // Run stop function on dismount
+  useEffect(() => {
+    return () => {
+      stopSequence();
+    };
+  }, [audio, bell1, bell2]);
+
+  // API call runs on mount
+  useEffect(() => {
+    axios
+      .get(API_URL)
+      .then((response) => {
+        let verseContent = response.data.verses.filter(
+          (verse) => verse.id === verseId
+        )[0];
+        setVerse(verseContent);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, [verseId]);
+
+  return (
+    <div className={styles.wrapper}>
+      <button onClick={exit} className={styles.back_btn}>
+        <Icon icon={back} width='40px' color='white' />
+      </button>
+      <h1>{verse && verse.short_desc}</h1>
+      <p className={`${styles.attribution} subtitle1`}>
+        {verse && verse["attribution_hr"]}
+      </p>
+      <div className={styles.time}>
+        {!meditating && <span>{length / 60000} minutes</span>}
+        {meditating && (
+          <span>
+            {remaining < 60000 && <Fragment>Less than 1 minute</Fragment>}
+            {remaining > 60000 && (
+              /* break out logic */
+              <Fragment>
+                {" "}
+                {Math.floor(remaining / 60000)} minute
+                {Math.floor(remaining / 60000) !== 1 && <span>s</span>}
+              </Fragment>
+            )}{" "}
+            remaining
+          </span>
+        )}
+      </div>
+      {!meditating && (
+        /* onMouseEnter and onMouseLeave instead */
+        <button
+          className={`${styles.play_btn} ${styles.meditate}`}
+          onMouseOut={() => props.setEngagement("")}
+          onMouseOver={() => {
+            props.setEngagement("press");
+          }}
+          onClick={() => playSequence()}
+        >
+          <Icon
+            icon={play}
+            width='80px'
+            color='white'
+            style={{ textAlign: "center" }}
+          />
+        </button>
+      )}
+      {meditating && (
+        <div className={`${styles.icon} ${styles.meditate}`}>
+          <img src={cross} alt='The Cross' width='100px' />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Meditation;
